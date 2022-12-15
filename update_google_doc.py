0bd7ef38eae1a354c8da86e8b578522380dbe852
kopyl <oleggggg1997@gmail.com>
2022-12-16 01:02:53 +0200

    Remove redundant things

--- update_google_doc.py
+++ update_google_doc.py
@@ -1,25 +1,17 @@
-# replace text in google doc
 import httplib2
 import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
 
-CREDENTIALS_FILE = 'key.json'  # имя файла с закрытым ключом
-# google_docs_id = "1KVeXRlieG-cZoUjeh2aV9JSq84WGnzag"
+CREDENTIALS_FILE = 'key.json'
 google_docs_id = "1hoP4acNyQxKpllDrogAo9SzERKxo5KSysxs3UiLqB1U"
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE,
-                                                               ['https://www.googleapis.com/auth/documents', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.readonly', 'https://www.googleapis.com/auth/documents.readonly',])
+permissions = [
+    'https://www.googleapis.com/auth/documents']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    CREDENTIALS_FILE, permissions
+)
 httpAuth = credentials.authorize(httplib2.Http())
 service = apiclient.discovery.build('docs', 'v1', http=httpAuth)
-
-
-# doc = service.documents().get(documentId=google_docs_id).execute()
-# doc_content = doc.get('body').get('content')
-
-# print(doc)
-
-# print(service.documents().create(body={}).execute())
-
 
 service.documents().batchUpdate(documentId=google_docs_id, body={
     "requests": [
@@ -29,7 +21,7 @@ service.documents().batchUpdate(documentId=google_docs_id, body={
                     "text": "Лоґлайн: «Відьма з картини полює на душу молодого водія Богдана, у сім'ї якого очікується поповнення»",
                     "matchCase": True
                 },
-                "replaceText": 'Ти там скоро, Альош? «Плейбой» мій надибав, чи що?\nAre you there soon, Alyosha? "Playboy" is my dick, or what?'
+                "replaceText": 'Hey, I am gay!'
             }
         }
     ]
